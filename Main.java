e48a5297cdf7ad4cc52f96cf356a60672f8ff8f5
pvijaykrishna549 <you@example.com>
2021-05-26 17:20:32 +0530

    UC1 & UC2 : Insertion in Binary Search Tree

---
+++ Main.java
@@ -0,0 +1,68 @@
+package com.bridgelabz.bstdemo;
+class BinarySearchTreeDemo {
+
+
+    class Node
+    {
+        int key;
+        Node left, right;
+
+        public Node(int item)
+        {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    BinarySearchTreeDemo()
+    {
+        root = null;
+    }
+
+    void insert(int key)
+    {
+        root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key)
+    {
+
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+
+        return root;
+    }
+
+    void inorder()
+    {
+        inorderRec(root);
+    }
+    void inorderRec(Node root)
+    {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        BinarySearchTreeDemo tree = new BinarySearchTreeDemo();
+
+        tree.insert(56);
+        tree.insert(30);
+        tree.insert(70);
+        tree.inorder();
+    }
+}
